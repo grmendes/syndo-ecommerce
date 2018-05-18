ab53b590d98cf555b177366b05528810785da5af
Artur Eiji <artur.eiji@gmail.com>
2018-05-18 15:52:02 -0300

    Adiciona integração com módulo de Clientes

---
+++ drupal/modules/syndo_ecommerce/src/Api/Client/ClientEndpointInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\syndo_ecommerce\Api\Client;
+
+interface ClientEndpointInterface
+{
+    const API_HOST = 'https://us-central1-first-try-18f38.cloudfunctions.net';
+
+    const CLIENT_ENDPOINT = '/clientsAPI/register';
+}
