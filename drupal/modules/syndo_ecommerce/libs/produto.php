d62fc4b869645c2655f5a8786ae302f8eab7c7cb
Gustavo <gustavo.cesar.nunes@gmail.com>
2018-05-25 07:39:06 -0300

    v0 sac

--- drupal/modules/syndo_ecommerce/libs/produto.php
+++ drupal/modules/syndo_ecommerce/libs/produto.php
@@ -1,7 +1,7 @@
 <?php
 
 //cadastrarProduto('CODVAZ', 'Meia de Lã Elza Frozen', 300, 10.9, 1.2, '', '', '', 'img.jpg', ['idcampo' => 100, 'valor' => 30]);
-
+//visualizarDadosProduto(132);
 
 function cadastrarProduto($codigo, $nome, $idcategoria, $preco, $peso, $dimensao_a, $dimensao_c, $dimensao_l, $imagem_url, $campos) {
 
@@ -49,4 +49,37 @@ function cadastrarProduto($codigo, $nome, $idcategoria, $preco, $peso, $dimensao
 
     return $jsonRet["id"]; 
 
+}
+
+function visualizarDadosProduto($codigo) {
+
+    // Get cURL resource
+    $curl = curl_init();
+    // Set some options - we are passing in a useragent too here
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'http://produtos.vitainformatica.com/api/produto?idempresa=1024',
+        CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+    ));
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Authorization: Bearer 90ddc7c5634095af7f087ed450ab4962b9f06dde9064e5ecb96bd0c3493af4bd')
+    );
+
+
+    // Send the request & save response to $resp
+    $resp = curl_exec($curl);
+    // Close request to clear up some resources
+    curl_close($curl);    
+
+
+    // Send the request
+
+    // Print the date from the response
+    $jsonRet = json_decode($resp, true);
+
+    $key = array_search($codigo, array_column($jsonRet, 'idproduto'));
+
+    return $jsonRet[$key];
 }
---
+++ drupal/modules/syndo_ecommerce/libs/sac.php
@@ -0,0 +1,71 @@
+<?php
+
+date_default_timezone_set("America/Sao_Paulo");
+
+//cadastrarTicketCliente('olar', 'eu', 'gnunes');
+//visualizarTicketsCliente('gnunes');
+
+function cadastrarTicketCliente($msg, $sender, $cliente) {
+    
+//    echo (date('Y-m-d') . 'T' . date('H:i'));
+
+    $postData = array(
+        'timestamp' => date('Y-m-d') . 'T' . date('H:i'),
+        'sender' => $sender,
+        'message' => $msg,
+    );
+
+    // Setup cURL
+    $ch = curl_init('https://centralatendimento-mc857.azurewebsites.net/tickets/c1f33357c6f9b3b559037f838c3501ae4eedb09e/' . $cliente);
+
+    $json = json_encode($postData);
+
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json')
+    );
+
+    // Send the request
+    $response = curl_exec($ch);
+
+    // Check for errors
+    if($response === FALSE){
+        die(curl_error($ch));
+    }
+
+    // Print the date from the response
+    $jsonRet = json_decode($response, true);
+
+    return $jsonRet; 
+
+}
+
+function visualizarTicketsCliente($cliente) {
+
+    // Get cURL resource
+    $curl = curl_init();
+    // Set some options - we are passing in a useragent too here
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://centralatendimento-mc857.azurewebsites.net/tickets/c1f33357c6f9b3b559037f838c3501ae4eedb09e/' . $cliente,
+        CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+    ));
+
+    // Send the request & save response to $resp
+    $resp = curl_exec($curl);
+    // Close request to clear up some resources
+    curl_close($curl);    
+
+
+    // Send the request
+
+    // Print the date from the response
+    $jsonRet = json_decode($resp, true);
+
+    var_dump($jsonRet);
+
+    return $jsonRet;
+}
+
