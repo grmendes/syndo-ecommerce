4e8eed4656b1a35e60357fa7bc8fa67d398569e3
Gustavo <gustavo.cesar.nunes@gmail.com>
2018-06-21 22:28:58 -0300

    cadastrarEntrega logistica.php

--- drupal/modules/syndo_ecommerce/libs/logistica.php
+++ drupal/modules/syndo_ecommerce/libs/logistica.php
@@ -1,6 +1,8 @@
 <?php
 
 //calcularFrete('SEDEX', 13065051, 13348863, 100, 'caixa', 2, 3, 4);
+//rastreiaPedido();
+cadastrarEntrega(1, 'PAC', 13065051, 13348863, 100, 'Caixa', 1, 1, 1); 
 
 function calcularFrete($tipoEntrega, $cepOrigem, $cepDestino, $peso, $tipoPacote, $comprimento, $altura, $largura) {
 
@@ -60,7 +62,51 @@ function rastreiaPedido($codigoRastreio) {
     // Print the date from the response
     $jsonRet = json_decode($resp, true);
 
-//    var_dump($jsonRet['preco']);
+    var_dump($jsonRet);
 
     return $jsonRet;
+}
+
+function cadastrarEntrega($idProduto, $tipoEntrega, $cepOrigem, $cepDestino, $peso, $tipoPacote, $altura, $largura, $comprimento) {
+    
+    $postData = array(
+        'idProduto' => $idProduto,
+        'tipoEntrega' => $tipoEntrega,
+        'cepOrigem' => $cepOrigem,
+        'cepDestino' => $cepDestino,
+        'peso' => $peso,
+        'tipoPacote' => $tipoPacote,
+        'altura' => $altura,
+        'largura' => $largura,
+        'comprimento' => $comprimento,
+        'apiKey' => '17b8e5f4-5fb6-593c-bf02-cdf57eab0c69',
+    );
+
+    // Setup cURL
+    $ch = curl_init('https://hidden-basin-50728.herokuapp.com/cadastrarentrega');
+
+    $json = json_encode($postData);
+
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'x-api-key: tmvcglg')
+    );
+
+    // Send the request
+    $response = curl_exec($ch);
+
+    // Check for errors
+    if($response === FALSE){
+        die(curl_error($ch));
+    }
+
+    // Print the date from the response
+    $jsonRet = json_decode($response, true);
+
+    var_dump($response);
+
+    return $jsonRet; 
 }
