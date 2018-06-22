c3ab5672be0c1646a14f6094c02993344dc944b2
Murilo Cruz <murilolcruz@gmail.com>
2018-06-22 05:15:15 -0300

    versão com erro no calculo do frete para o carrinho

--- drupal/modules/syndo_ecommerce/src/Controller/CartController.php
+++ drupal/modules/syndo_ecommerce/src/Controller/CartController.php
@@ -4,7 +4,10 @@ namespace Drupal\syndo_ecommerce\Controller;
 
 use Drupal;
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\user\PrivateTempStoreFactory;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Session\SessionManager;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
+use Drupal\Core\TempStore\TempStoreException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
@@ -12,34 +15,61 @@ class CartController extends ControllerBase
 {
   protected $tempStore;
 
-    // Pass the dependency to the object constructor
-    public function __construct(PrivateTempStoreFactory $temp_store_factory) {
-      // For "mymodule_name," any unique namespace will do
-      $this->tempStore = $temp_store_factory->get('syndo-ecommerce');
+  /**
+   * @var PrivateTempStoreFactory
+   */
+  protected $userPrivateTempstore;
 
-      if($this->tempStore->get('cart_items') == NULL) {
-        $this->tempStore->set('cart_items', ['idproduto1' =>['nome' => 'Meia extra confortável', 'quantidade' => 15, 'preco' => 'R$123,45'], 'idproduto2' =>['nome' => 'Meia meh dinamica', 'quantidade' => 2, 'preco' => 'R$10,99'] ]);
-      }
-    }
+  /**
+   * @var SessionManager
+   */
+  protected $sessionManager;
 
-    // Uses Symfony's ContainerInterface to declare dependency to be passed to constructor
-    public static function create(ContainerInterface $container) {
-        return new static(
-            $container->get('user.private_tempstore')
-        );
-    }
+  /**
+   * @var AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * @param PrivateTempStoreFactory $temp_store_factory
+   */
+  public function __construct(
+      PrivateTempStoreFactory $user_private_tempstore,
+      SessionManager $session_manager,
+      AccountInterface $current_user
+  ) {
+      $this->sessionManager = $session_manager;
+      $this->currentUser = $current_user;
+      $this->userPrivateTempstore = $user_private_tempstore->get('syndo_ecommerce');
+  }
+
+  public static function create(ContainerInterface $container) {
+      return new static(
+          $container->get('user.private_tempstore'),
+          $container->get('session_manager'),
+          $container->get('current_user')
+      );
+  }
 
     public function view() {
-        $cart_items = $this->tempStore->get('cart_items');
+        if ($this->currentUser->isAnonymous() && !isset($_SESSION['session_started'])) {
+            $_SESSION['session_started'] = TRUE;
+            $this->sessionManager->start();
+        }
 
-        $frete_form = Drupal::formBuilder()->getForm('Drupal\syndo_ecommerce\Form\CartDeliveryForm');
+        $cart_items = $this->userPrivateTempstore->get('cart_items') ?? [];
+
+        $frete_form = Drupal::formBuilder()->getForm('Drupal\syndo_ecommerce\Form\CartDeliveryForm', $cart_items);
 
 
+
+        // var_dump($cart_items);die();
         $element = array(
             '#type' => 'container',
             'lista' => [
                 '#type' => 'table',
-                '#header' => ['Produto', 'Quantidade', 'Preço'],
+                //'#header' => ['Produto', 'Quantidade', 'Preço'],
+                '#header' => ['Quantidade'],
                 '#rows' => $cart_items
             ],
             'frete' => $frete_form,
--- drupal/modules/syndo_ecommerce/src/Form/CartDeliveryForm.php
+++ drupal/modules/syndo_ecommerce/src/Form/CartDeliveryForm.php
@@ -43,7 +43,7 @@ class CartDeliveryForm extends FormBase {
     if ($form_state->has('frete')) {
       $frete = $form_state->get('frete');
       $form['valor_calculado'] = array(
-        '#markup' => "<p>Preço: R\$".$frete['preco']."</p><p>Prazo: ".$frete."['prazo']</p>",
+        '#markup' => "<p>Preço: R\$".$frete['preco']."</p><p>Prazo: ".$frete['prazo']."</p>",
       );
     }
     return $form;
@@ -54,18 +54,20 @@ class CartDeliveryForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $cep = $form_state->getValue('zipcode');
     $entrega = $form_state->getValue('entrega');
+    $cart_items = $form_state->get('args')[0];
+    $resultadoFrete = array('preco' => 0, 'prazo'=> 0);
+    foreach ($cart_items as $key => $value) {
+      $dadosProduto = visualizarDadosProduto($key);
+      $frete = calcularFrete($entrega, "13083-872", $cep, 1000, 'Caixa', 10, 10, 10);
+      foreach ($fretes as $tipoEntrega => $dadosFrete) {
+          $resultadoFretes[$tipoEntrega]["preco"] += intval($dadosFrete[$tipoEntrega]["preco"]) * $value["quantidade"];
+          if(intval($resultadoFretes[$tipoEntrega]["prazo"]) > $dadosFrete[$tipoEntrega]["prazo"]) {
+              $dadosFrete[$tipoEntrega]["prazo"] = intval($resultadoFretes[$tipoEntrega]["prazo"]);
+          }
+      }
+    }
 
-    // foreach ($cart_items as $key => $value) {
-    //   // $dadosProduto = visualizarDadosProduto($key);
-    //   // $fretes = calculafretes( $key...$cep_entrega...$dadosProduto[].......);
-    //   // foreach ($fretes as $tipoEntrega => $dadosFrete) {
-    //   //     $resultadoFretes[$tipoEntrega]["preco"] += intval($dadosFrete[$tipoEntrega]["preco"]) * $value["quantidade"];
-    //   //     if(intval($resultadoFretes[$tipoEntrega]["prazo"] > $dadosFrete[$tipoEntrega]["prazo"])
-    //   //         $dadosFrete[$tipoEntrega]["prazo"] = intval($resultadoFretes[$tipoEntrega]["prazo"]
-    //   // }
-    // }
 
-    $frete = calcularFrete($entrega, "13083-872", $cep, 1000, 'Caixa', 10, 10, 10);
 
 
     $form_state->set('frete', $frete);
